ccb000f3d6cbb5ce6c14cd5f1e0686d60f74627a
Michael Michael <michmike@cs.stanford.edu>
2017-01-20 19:34:23 -0600

    Add files via upload

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
-# launcher.sh
-# requires crontab; Install using: sudo apt-get install gnome-schedule
-# Create crontab using: @reboot sh /home/pi/raspberry-pi-q/launcher.sh
-# Check scheduled tasks using: crontab -l
+#!/bin/bash
 
 cd /
 cd home/pi/Raspberry-PI-Q 
 sudo git pull
 sudo python3 email_IP_address.py
-#sudo git clone https://github.com/michmike/Raspberry-PI-Q.git  
 cd /

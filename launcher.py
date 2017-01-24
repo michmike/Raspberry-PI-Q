fe18d92cf161b7a5b9903499145d7ebb9be20aa5
Michael Michael <michmike@cs.stanford.edu>
2017-01-24 11:23:02 -0600

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -2,4 +2,5 @@ import os
 
 os.chdir("/home/pi/Raspberry-PI-Q")
 os.system("sudo git pull")
+os.system("sudo cp /home/pi/Raspberry-PI-Q/index.html /var/www/html/.")
 os.system("sudo python3 email_IP_address.py")
